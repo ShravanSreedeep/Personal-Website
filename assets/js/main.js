5f50e25f3fae9ff07966dd242e27e9db77c8132b
ARTHEROOV <58596722+ShravanSreedeep@users.noreply.github.com>
2021-06-25 19:22:12 +0530

    second

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,26 +29,26 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
-    duration: 3000,
+    duration: 2000,
     reset: true
 });
 
 /*SCROLL HOME*/
 sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal('.button',{delay: 100}); 
+sr.reveal('.home__img',{delay: 200}); 
+sr.reveal('.home__social-icon',{ interval: 100}); 
 
 /*SCROLL ABOUT*/
 sr.reveal('.about__img',{}); 
 sr.reveal('.about__subtitle',{delay: 100}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal('.about__text',{delay: 200}); 
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle',{}); 
 sr.reveal('.skills__text',{}); 
 sr.reveal('.skills__data',{interval: 100}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal('.skills__img',{delay: 300});
 
 /*SCROLL WORK*/
 sr.reveal('.work__img',{interval: 100}); 
